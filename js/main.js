f19735944257087d1c381416c699cda954bb3e6e
Extended.kid <iljalobossok@gmail.com>
2021-03-09 15:10:49 +0200

    create: add to previous commit files

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.hotel-slider__button_next',
+    prevEl: '.hotel-slider__button_prev',
+  },
+});
