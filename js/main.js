b1363f86cf047a408e06bba9aa9cc74631ef8167
Extended.kid <iljalobossok@gmail.com>
2021-03-10 21:42:14 +0200

    create: about section with adaptive

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper-container', {
+const hotel_swiper = new Swiper('.hotel__swiper', {
   // Optional parameters
   loop: true,
 
@@ -14,14 +14,30 @@ const swiper = new Swiper('.swiper-container', {
   },
 });
 
+const about_swiper = new Swiper('.about__swiper', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.about-button_next',
+    prevEl: '.about-button_prev',
+  },
+});
+
 
 document.onkeydown = function(e) {
     switch (e.keyCode) {
         case 37:
-            swiper.slidePrev(300, true)
+            hotel_swiper.slidePrev(300, true)
             break;
         case 39:
-            swiper.slideNext(300, true)
+            hotel_swiper.slideNext(300, true)
             break;
     }
 };
