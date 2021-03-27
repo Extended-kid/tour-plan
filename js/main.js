5f20b22eac1c96b6dee42045831e950b80f9c33d
Extended.kid <iljalobossok@gmail.com>
2021-03-27 08:41:01 +0200

    fix: js error and packages card button

--- js/main.js
+++ js/main.js
@@ -41,45 +41,7 @@ document.onkeydown = function(e) {
             break;
     }
 };
-ymaps.ready(init);
-  function init() {
-    var myMap = new ymaps.Map("map", {
-      center: [7.89096874, 98.29429420],
-      zoom: 17
-    }); 
-    HintLayout = ymaps.templateLayoutFactory.createClass( "<div class='my-hint'>" +
-            "<b>{{ properties.object }}</b><br />" +
-            "{{ properties.address }}" +
-            "</div>", {
-                // Определяем метод getShape, который
-                // будет возвращать размеры макета хинта.
-                // Это необходимо для того, чтобы хинт автоматически
-                // сдвигал позицию при выходе за пределы карты.
-                getShape: function () {
-                    var el = this.getElement(),
-                        result = null;
-                    if (el) {
-                        var firstChild = el.firstChild;
-                        result = new ymaps.shape.Rectangle(
-                            new ymaps.geometry.pixel.Rectangle([
-                                [0, 0],
-                                [firstChild.offsetWidth, firstChild.offsetHeight]
-                            ])
-                        );
-                    }
-                    return result;
-                }
-            }
-        );
 
-    var myPlacemark = new ymaps.Placemark([7.89096874, 98.29429420], {
-        object: "GRAND HILTON HOTEL"
-    }, {
-        hintLayout: HintLayout
-    });
-
-    myMap.geoObjects.add(myPlacemark);
-}
 
 
 
